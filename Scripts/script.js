07e651b770b3d28a354d2073c267709447f9707b
Shekh Mahdi Mesbah <mahdisheikh1624@gmail.com>
2026-02-24 12:39:37 +0600

    some functionality added

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,6 @@
 let interviewList = [];
 let rejectionList = [];
+let currentTab = 'tabAll';
 
 const totalJobs = document.getElementById('totalJobs'); 
 const totalInterview = document.getElementById('totalInterview');
@@ -11,6 +12,9 @@ const tabInterview = document.getElementById('tabInterview');
 const tabRejected = document.getElementById('tabRejected');
 
 const jobList = document.getElementById('jobList');
+
+//Main container
+const mainContainer = document.querySelector('main');
 
 function allJobs(){
     totalJobs.innerHTML = jobList.children.length;
@@ -33,3 +37,37 @@ function toggleStyle(id){
     selected.classList.remove('btn-soft');
 }
 allJobs();
+
+
+mainContainer.addEventListener('click', function(event){
+    if (event.target.classList.contains('intterview-btn')) {
+        const card = event.target.parentNode.parentNode;
+        // console.log(card);
+
+        const statusBtn = card.querySelector('.status-btn');
+        statusBtn.textContent = 'INTERVIEW';
+        statusBtn.className = 'status-btn btn btn-sm btn-outline btn-success rounded-md';
+
+        const jobObject = {
+            company: card.querySelector('h3').textContent,
+            position: card.querySelector('p').textContent,
+            location: card.querySelectorAll('span')[0].textContent,
+            type: card.querySelectorAll('span')[2].textContent,
+            salary: card.querySelectorAll('span')[4].textContent,
+            status: 'Interview'
+        };
+
+        const interviewGiven = interviewList.find(item => item.company === jobObject.company);
+
+        if(!interviewGiven) interviewList.push(jobObject);
+
+        rejectionList = rejectionList.find(item => item.company != jobObject.company);
+        
+        // console.log(interviewList);
+
+        if(currentTab === 'tabRejected') {
+            renderRejected();
+            //create the render function after coming home
+        }
+    }
+})
